061a4cb60fb1169eb1cfacdfda0093d25b0b6d0a
maqsudamadrahimova.9@gmail.com <maqsudamadrahimova.9@gmail.com>
2026-03-18 11:49:59 +0500

    task

--- 6-oy-2-dars.sql
+++ 6-oy-2-dars.sql
@@ -1,1 +1,22 @@
-create database Student;
+create database talabalar;
+
+create table if not exists talabalar(
+    id serial primary key,
+    name varchar not null,
+    birth_date date not null,
+    gender varchar(10) not null,
+    grade int not null,
+    address text not null
+);
+
+
+SELECT * FROM talabalar ORDER BY grade DESC;
+
+SELECT * FROM talabalar ORDER BY grade ASC;
+
+create table if not exists kurslar(
+    id serial primary key,
+    kurs_nomi varchar not null,
+    davomiyligi int not null,
+    narxi int not null
+);
