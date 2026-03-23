eeaae2e8712598d026b2acba07b1127b07626ede
maqsudamadrahimova.9@gmail.com <maqsudamadrahimova.9@gmail.com>
2026-03-23 09:48:58 +0500

    task

--- 6-oy-2-dars.sql
+++ 6-oy-2-dars.sql
@@ -12,6 +12,26 @@ create table if not exists talabalar(
 select * from talabalar
 LIMIT 10 OFFSET 5;
 
+select birth_date AS tugilgan_kun
+from talabalar;
+
+select shahar, count(*) AS user_count
+from talabalar
+GROUP BY shahar
+HAVING count(*) > 1;
+
+
+SELECT name FROM talabalar
+UNION
+SELECT name FROM kurslar;
+
+
+SELECT *
+FROM talabalar
+WHERE score BETWEEN 70 AND 90 OR city = 'Tashkent';
+
+SELECT *FROM students
+WHERE name ILIKE 'sh%' OR name ILIKE '%sh';
 
 SELECT * FROM talabalar ORDER BY grade DESC;
 
